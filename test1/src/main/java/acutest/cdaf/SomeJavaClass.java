205afec78f95a84dd92993b76dc6cba84934be12
Ali Warsame <ali.warsame@travisperkins.co.uk>
2018-02-14 09:57:10 +0000

    First commit

---
+++ test1/src/main/java/acutest/cdaf/SomeJavaClass.java
@@ -0,0 +1,11 @@
+package acutest.cdaf;
+
+/**
+ * Created by aliwarsame on 14/02/2018.
+ */
+public class SomeJavaClass {
+
+    public void testOneMethod(){
+        System.out.println("This is Test1 class method");
+    }
+}
